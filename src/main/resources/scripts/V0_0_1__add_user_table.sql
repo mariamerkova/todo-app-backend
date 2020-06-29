4520f5721dbc51a0bac1fe5a115a31174fda01e6
Maria Merkova <maria.h.merkova@gmail.com>
2020-06-29 15:30:02 +0300

    Adding flyway

---
+++ src/main/resources/scripts/V0_0_1__add_user_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE USER(
+id bigint(20) NOT NULL AUTO_INCREMENT,
+username varchar(100),
+password varchar(100),
+is_active tinyint(4),
+roles varchar(100),
+CONSTRAINT PK PRIMARY KEY (id)
+);
